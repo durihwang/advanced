f8829fcae15a37d2cb8117e3177ea7204979e6ed
황두리 <hwangduli0515@gmail.com>
2022-01-11 23:08:15 +0900

    first commit

---
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,13 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+
+    }
+}
