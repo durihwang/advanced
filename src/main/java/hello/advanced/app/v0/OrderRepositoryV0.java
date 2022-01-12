2d0b23500eac980057860233cc526b5596a73905
황두리 <hwangduli0515@gmail.com>
2022-01-12 21:55:53 +0900

    v0 생성

--- src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -9,5 +9,17 @@ public class OrderRepositoryV0 {
 
     public void save(String itemId) {
 
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
