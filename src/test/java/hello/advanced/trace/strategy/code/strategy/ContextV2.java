aeb8b3c314775b4bbd8fda627fa731d270d9c443
황두리 <hdr@gabia.com>
2022-10-17 19:49:13 +0900

    전략 패턴 공부

---
+++ src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
@@ -0,0 +1,24 @@
+package hello.advanced.trace.strategy.code.strategy;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 전략을 파라미터로 전달 받는 방식
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class ContextV2 {
+
+    public void execute(Strategy strategy) {
+        long startTime = System.currentTimeMillis();
+
+        // 비즈니스 로직
+        strategy.call();    // 위임
+        // 비즈니스 로직
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+}
